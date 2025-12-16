20013f3970d0fdc828b89ad94c338f4e9ff10df0
Laila Case <laila.case@vodafone.com>
2025-12-16 13:19:56 +0000

    Add list functionality for events

--- app/(tabs)/events/index.tsx
+++ app/(tabs)/events/index.tsx
@@ -1,3 +1,34 @@
-export default function IndexEventScreen() {
-  return null;
+import { getDocs, collection } from "firebase/firestore";
+
+import { db } from "@/src/core/firebase/client";
+import { useState, useEffect } from "react";
+import { Text, View } from "react-native";
+
+export default function EventsList() {
+  const [events, setEvents] = useState<any[]>([]);
+
+  useEffect(() => {
+    async function loadEvents() {
+      try {
+        const snap = await getDocs(collection(db, "events"));
+        const evts = snap.docs.map((d) => ({ id: d.id, ...(d.data() as any) }));
+        console.log("Loaded events:", evts);
+        setEvents(evts);
+      } catch (e: any) {
+        console.error("Failed to load events", e);
+      }
+    }
+    loadEvents();
+  }, []);
+
+  return (
+    <View>
+      {events.map((event) => (
+        <View key={event.id}>
+          <Text>{event.title}</Text>
+          <Text>{event.description}</Text>
+        </View>
+      ))}
+    </View>
+  );
 }
